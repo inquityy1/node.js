bb5258300f65586924c3a7fecccf16f02319b6c1
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-01-28 15:26:45 +0100

    Extracting Data

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-const ecpress = require('express');
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use((req, res, next) => {
 	res.setHeader('Content-Type', 'text/html');
@@ -8,7 +11,8 @@ app.use((req, res, next) => {
 });
 
 app.use((req, res, next) => {
-	res.send('<h1>Hello World!</h1>');
+	const userName = req.body.username || 'Unknown user';
+	res.send(`<h1>Hi ${userName}</h1><form method="POST" action="/"><input name="username" type="text"><button type="submit">send</button></form>`);
 });
 
 app.listen(3000);
