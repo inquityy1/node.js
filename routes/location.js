856d5a600a91cfef35f1b87daa2dec16b1a7f843
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-01-28 19:28:08 +0100

    adding the get location route

--- routes/location.js
+++ routes/location.js
@@ -7,14 +7,24 @@ const locationStorage = {
 };
 
 router.post('/add-location', (req, res, next) => {
+	const id = Math.random();
 	locationStorage.locations.push({
-		id: Math.random(),
+		id: id,
 		address: req.body.address, 
 		coords: {lat: req.body.lat, lng: req.body.lng}
 	});
-	res.json({message: 'Stored location!'});
+	res.json({message: 'Stored location!', locId: id});
 });
 
-router.post('/location', (req, res, next) => {});
+router.post('/location', (req, res, next) => {
+	const locationId = +req.params.lid;
+	const location = locationStorage.location.find(loc => {
+		return loc.id === locationId;
+	});
+	id (!location) {
+		return res.status(404).json({message: 'Not found!'});
+	}
+	res.json({address: location.address, coordinates: location.coords});
+});
 
 module.exports = router;
